ead57c9072bffaed370c7a1031b21ebe4a3868f7
Martin <martin.stankard@hotmail.com>
2020-11-09 13:22:58 -0700

    updated js/config.js.congito bits

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_MqcFcRRID', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3r0n3fe4a0h29onsq4svc1focb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
